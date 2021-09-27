a426d47d6408211d2fbbcd8d9f19d180c3f36ad1
Matthieu Foucault <matthieu@button.is>
2021-09-27 12:25:45 -0700

    feat: add verify_function_not_present change

--- generator-cas-app/generators/app/templates/schema/deploy/schemas/main.sql
+++ generator-cas-app/generators/app/templates/schema/deploy/schemas/main.sql
@@ -3,6 +3,7 @@
 begin;
 
 create schema <%= schemaName %>;
+grant usage on schema <%= schemaName %> to <%= roles %>;
 comment on schema <%= schemaName %> is 'The main schema for the <%= projectName %> application.';
 
 commit;
--- generator-cas-app/generators/app/templates/schema/deploy/schemas/private.sql
+++ generator-cas-app/generators/app/templates/schema/deploy/schemas/private.sql
@@ -3,6 +3,8 @@
 begin;
 
 create schema <%= schemaName %>_private;
+-- If you have a "guest" role, you may not want to grant access to the private schema to it.
+grant usage on schema <%= schemaName %>_private to <%= roles %>;
 comment on schema <%= schemaName %>_private is 'The private schema for the <%= projectName %> application. It contains utility functions which should not be available directly through the API.';
 
 commit;
