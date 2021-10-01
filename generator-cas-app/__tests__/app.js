05acf503ead6261e2d168c89a7fc1550bf9149c6
Matthieu Foucault <matthieu@button.is>
2021-10-01 09:16:26 -0700

    chore: update dependencies and linter rules

--- generator-cas-app/__tests__/app.js
+++ generator-cas-app/__tests__/app.js
@@ -4,17 +4,17 @@ const helpers = require("yeoman-test");
 const { spawnSync } = require("child_process");
 
 describe("generator-cas-app:app", () => {
-  beforeAll(() => {
-    return helpers.run(path.join(__dirname, "../generators/app")).withPrompts({
+  beforeAll(() =>
+    helpers.run(path.join(__dirname, "../generators/app")).withPrompts({
       nodejs: "14.17.6",
       yarn: "1.22.5",
       postgres: "12.6",
       python: "3.9.2",
       projectName: "abcd",
       committerEmail: "foo@bar.com",
-      committerName: "Foo Bar"
-    });
-  });
+      committerName: "Foo Bar",
+    })
+  );
 
   it("creates files", () => {
     assert.file([".tool-versions"]);
@@ -24,7 +24,7 @@ describe("generator-cas-app:app", () => {
 
   it("passes database unit tests", () => {
     const { status } = spawnSync("make", ["db_unit_tests"], {
-      stdio: "inherit"
+      stdio: "inherit",
     });
 
     assert.equal(status, 0);
