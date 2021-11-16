49c9de282301059e0b438b5d8e7bddf4a729d302
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2021-11-15 16:20:59 -0800

    refactor: exporting the new effect

--- packages/sso-react/src/ssoReact.tsx
+++ packages/sso-react/src/ssoReact.tsx
@@ -1,4 +1,5 @@
 import SessionTimeoutHandler from "./SessionTimeoutHandler";
 import LogoutWarningModal from "./LogoutWarningModal";
+import throttleEventsEffect from "./throttleEventsEffect";
 
-export { SessionTimeoutHandler, LogoutWarningModal };
+export { SessionTimeoutHandler, LogoutWarningModal, throttleEventsEffect };
