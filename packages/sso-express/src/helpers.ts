f5c6dcafef1afe42f9126b9865a683b4f10342e7
Matthieu Foucault <matthieu@button.is>
2021-11-23 16:44:05 -0800

    fix: getSessionRemainingTime uses refreshToken expiry

--- packages/sso-express/src/helpers.ts
+++ packages/sso-express/src/helpers.ts
@@ -2,10 +2,19 @@ import type { Request } from "express";
 
 export const isAuthenticated = (req: Request) => !!req.session?.tokenSet;
 
+const decodeJwt = (token: string) => {
+  const [header, payload, signature] = token.split(".");
+  return {
+    header: JSON.parse(Buffer.from(header, "base64").toString("utf8")),
+    payload: JSON.parse(Buffer.from(payload, "base64").toString("utf8")),
+    signature: signature,
+  };
+};
+
 export const getSessionRemainingTime = (req: Request) => {
   if (!isAuthenticated(req)) {
     return 0;
   }
-  const { expires_at } = req.session.tokenSet;
-  return Math.round(expires_at - Date.now() / 1000);
+  const refreshToken = decodeJwt(req.session.tokenSet.refresh_token);
+  return Math.round(refreshToken.payload.exp - Date.now() / 1000);
 };
